840d4e18735ff4179a55e1006f1bcbdd94687648
fhochleitner <felix.hochleitner@outlook.com>
2023-11-15 10:04:17 +0100

    fixes #20 RBAC collector removes labels from labels configMap
    retain labels and annotations on rbac-configmap by patching the resource instead of using update
    
    Signed-off-by: fhochleitner <felix.hochleitner@outlook.com>

--- util/util.go
+++ util/util.go
@@ -2,10 +2,13 @@ package util
 
 import (
 	"context"
-
-	yaml "gopkg.in/yaml.v3"
-	v1 "k8s.io/api/core/v1"
+	"fmt"
+	"gopkg.in/yaml.v3"
 	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
+	"strings"
+
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
@@ -15,7 +18,19 @@ func WriteConfigmap(clientset *kubernetes.Clientset, permission map[string]map[s
 	if err != nil {
 		return err
 	}
+	data := strings.Replace(string(permissions), "\n", "\\n", -1)
+	patch := []byte(fmt.Sprintf(`{"data":{"labels.yaml": "%s"}}`, data))
 
+	_, err = clientset.CoreV1().ConfigMaps(c.CMNamespace).Patch(context.Background(), c.CMName, types.MergePatchType, patch, metav1.PatchOptions{})
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return createConfigMap(clientset, err, c, permissions)
+		}
+	}
+	return nil
+}
+
+func createConfigMap(clientset *kubernetes.Clientset, err error, c Config, permissions []byte) error {
 	cm := v1.ConfigMap{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      c.CMName,
@@ -27,19 +42,12 @@ func WriteConfigmap(clientset *kubernetes.Clientset, permission map[string]map[s
 		},
 		BinaryData: nil,
 	}
-
-	_, err = clientset.CoreV1().ConfigMaps(c.CMNamespace).Update(context.Background(), &cm, metav1.UpdateOptions{})
+	_, err = clientset.CoreV1().ConfigMaps(c.CMNamespace).Create(context.Background(), &cm, metav1.CreateOptions{})
 	if err != nil {
-		if errors.IsNotFound(err) {
-			_, err = clientset.CoreV1().ConfigMaps(c.CMNamespace).Create(context.Background(), &cm, metav1.CreateOptions{})
-			if err != nil {
-				return err
-			}
-			return nil
-		}
 		return err
 	}
 	return nil
+	return err
 }
 
 func MapsEqual(m1, m2 map[string]map[string]bool) bool {
